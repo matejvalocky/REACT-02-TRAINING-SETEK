cb68f01e66a1eb480945a19f9225d619f6040d6b
Matej Valocký <matej.valocky@gmail.com>
2023-05-17 23:37:43 +0200

    2. React 2 - Stylování formuláře

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from 'react'
 const App = () => {
   return <article>
     <form>
-        <input type="text" id='userName' placeholder='Meno' />
+        <input className='userName' type="text" id='userName' placeholder='Meno' />
         <input type="submit" value="Registrovať sa" />
     </form>
-    <a href="https://www.youtube.com/watch?v=NYf9hNqADYs&list=PLQ8x_VWW6Aktmp_v1eYcCBbO5GovP_sHl&ab_channel=David%C5%A0etek-Hacknisvoubudoucnost" target='_blank'>Odkaz na video</a>
+    <a href="https://youtu.be/YgIu54eSMRo?list=PLQ8x_VWW6Aktmp_v1eYcCBbO5GovP_sHl" target='_blank'>Odkaz na video</a>
   </article>
 }
 
