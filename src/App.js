8f6f33e2ff044cc489d57df9263a79a2996a3457
Matej Valocký <matej.valocky@gmail.com>
2023-05-18 22:55:00 +0200

    4. React 2 - useState a formulář

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useState } from "react"
 
 const App = () => {
+  const [firstName, setFirstName] = useState("")
+
+
   const formSubmit = (event) => {
     event.preventDefault() // vypnutie refreshu pri zadaní hodnoty do formulara a stlačení submit
     console.log("Formuár bol odoslaný");
@@ -10,10 +13,14 @@ const App = () => {
 
   return <article>
     <form>
-        <input className='userName' type="text" id='userName' placeholder='Meno' />
+        <input className='userName' 
+          type="text" 
+          placeholder='Meno' 
+          value={firstName}
+        />
         <input onClick={formSubmit} type="submit" value="Registrovať sa" />
     </form>
-    <a href="https://youtu.be/mjw1AsQgxEM?list=PLQ8x_VWW6Aktmp_v1eYcCBbO5GovP_sHl" target='_blank'>Odkaz na video</a>
+    <a href="https://youtu.be/6G1jqMUqC08?list=PLQ8x_VWW6Aktmp_v1eYcCBbO5GovP_sHl" target='_blank'>Odkaz na video</a>
   </article>
 }
 
