2c37a7b66235f8259e4afcd575bb18bf89d0fb46
Matej Valocký <matej.valocky@gmail.com>
2023-05-26 22:03:57 +0200

    10. React 2 - Rest operátor

--- src/App.js
+++ src/App.js
@@ -4,70 +4,57 @@ import { useState } from "react"
 
 
 
-// SPREAD OPERATOR 
-// toto dokáže:
-// Spájanie polí
-
-// const studentsGroup = ["David", "Jana", "Martina"]
-// const allStudents = ["Harry", "Ron", ...studentsGroup, "Hermiona"]
-// console.log(allStudents)
-//
-
-// ARGUMENTY FUNKCII
-// vynásobí všetky prvky v poli
-// const numbers = [1, 5, 3]
-
-// const multipleNumbers = (a, b, c) => {
-//   console.log(a * b * c)
-// }
-
-// multipleNumbers(1, 2, 3)
-// multipleNumbers(...numbers) 
-
-// const App = () => {
-//   const [firstName, setFirstName] = useState("")
-//   const [names, setNames] = useState([])
-//
-
-
-// KOPIROVANIE POLI
-// const students1 = ["Jan", "Monika", "Dalibor"]
-// const students2 = students1
-// console.log(students2)
-
-//pridaj posledný prvok
-// students2.push("Pavol")
-// console.log(students2)
-
-// console.log(students1) 
-// aj s Pavlom // urobili sme odkaz zo students 2 na students1
-
-// const students3 = ["Jan", "Monika", "Dalibor"]
-// const students4 = [...students3]
-
-
-// students4.push("Pavol")
-
-// console.log(students3)
-// // vypise bez Pavla
-
-// console.log(students4) 
-// // vypise s Pavlom
+// REST OPERATOR
+const sum = (...allNumbers) => {
+  let result = 0
+  for (const oneNumber of allNumbers){
+    // console.log(oneNumber)
+    // result = result + oneNumber
+    result += oneNumber
+  }
+  return result
+}
 
 
 
-// SPOJOVANIE POLI = CONCATENATE = chceme ich spojit dokopy
-// const names1 = ["David", "Martin"]
-// const names2 = ["Jana", "Pavla"]
+// je jedno koľko čísel tam pošleme, vždy to vezme všetky, funguje
+// to univerzálne = REST OPERATOR
+console.log(sum(1, 2))
+console.log(sum(1, 2, 3))
+console.log(sum(1, 2, 5, 10))
+console.log(sum(1, 2, 10, 10, 10))
 
-// const newArray1 = names1.concat(names2)
 
-// console.log(newArray1) // vypíše David, Martin, Jana, Pavla
 
-// const newArray2 = [...names1, "Dana", ...names2]
-// // newArray2.push("Teodor")
-// // console.log(newArray1)
-// console.log(newArray2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -117,7 +104,7 @@ const App = () => {
 
 
 
-    <a href="https://youtu.be/qLoxxy1M2W4?list=PLQ8x_VWW6Aktmp_v1eYcCBbO5GovP_sHl" target='_blank'>Odkaz na video</a>
+    <a href="https://youtu.be/B97fzZwqSHU?list=PLQ8x_VWW6Aktmp_v1eYcCBbO5GovP_sHl" target='_blank'>Odkaz na video</a>
   </article>
 }
 
