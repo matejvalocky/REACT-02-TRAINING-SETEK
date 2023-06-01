1e99402a3fbdba78bd6516c8d5f15b6abc81c3fb
Matej Valocký <matej.valocky@gmail.com>
2023-06-01 22:15:33 +0200

    13. React 2 -  odeslání formuláře

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState } from "react"
 
 const App = () => {
+  const[fullName, setFullName] = useState("")
+  const [email, setEmail] = useState("")
+  const[users, setUsers] = useState([])
+
+  const formSubmit = (event) => {
+    event.preventDefault()
+    console.log("form bol odoslaný")
+  }
+
+
+
   return <article>
-    <form>
+    <form onSubmit={formSubmit}>
       <input className="userInfo" type="text" placeholder="Meno" />
       <input className="userInfo" type="email" placeholder='Email' />
       <input type="submit" />
